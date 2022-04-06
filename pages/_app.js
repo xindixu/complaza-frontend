aea4a7855ac08ec6b51efabbd91f433415b9c318
Xindi Xu <xindixu0@gmail.com>
2022-04-05 23:06:51 -0400

    feat: useContext to get authenticated user data

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
+import { useEffect, useState } from "react"
 import Head from "next/head"
 import "../styles/globals.css"
-import { Layout, Menu } from "antd"
+import { Layout } from "antd"
 import Amplify from "aws-amplify"
 import Navbar from "../components/navbar"
-
+import AuthContext from "../context/auth"
+import { getCurrentUser } from "../lib/auth"
 import awsconfig from "../src/aws-exports"
 
 const { Content } = Layout
 
 Amplify.configure({ ...awsconfig, ssr: true })
+
 function App({ Component, pageProps }) {
+  const [currentUser, setCurrentUser] = useState({})
+  useEffect(() => {
+    setCurrentUser(getCurrentUser())
+  }, [])
+
   return (
     <>
       <Head>
@@ -17,14 +25,16 @@ function App({ Component, pageProps }) {
         <meta name="description" content="Compare products!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
-        <Navbar />
-        <Content style={{ padding: "0 50px", marginTop: 64, height: "100%" }}>
-          <div style={{ padding: 24, minHeight: 380 }}>
-            <Component {...pageProps} />
-          </div>
-        </Content>
-      </Layout>
+      <AuthContext.Provider value={currentUser}>
+        <Layout>
+          <Navbar />
+          <Content style={{ padding: "0 50px", marginTop: 64, height: "100%" }}>
+            <div style={{ padding: 24, minHeight: 380 }}>
+              <Component {...pageProps} />
+            </div>
+          </Content>
+        </Layout>
+      </AuthContext.Provider>
     </>
   )
 }
