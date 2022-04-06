183db250366ff17a01a1b139fb55e4dca2651c52
Xindi Xu <xindixu0@gmail.com>
2022-04-05 20:31:42 -0400

    feat(session): create login/sign up UI

---
+++ components/forms/session.js
@@ -0,0 +1,86 @@
+import React from "react"
+import PropTypes from "prop-types"
+import NextLink from "next/link"
+import { Form, Input, Button, Typography } from "antd"
+
+const { Text, Link } = Typography
+
+export const LOGIN = "LOGIN"
+export const SIGNUP = "SIGNUP"
+const BUTTON_TEXT = {
+  LOGIN: "Login",
+  SIGNUP: "Sign up",
+}
+
+function NewSession({ type }) {
+  const onFinish = (values) => {
+    console.log("Success:", values)
+  }
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo)
+  }
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 8 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item label="Email" name="email" rules={[{ required: true }, { type: "email" }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Password" name="password" rules={[{ required: true }]}>
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        {type === LOGIN ? (
+          <Text type="secondary">
+            New to Complaza? Please{" "}
+            <NextLink href="/signup" passHref>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link>{BUTTON_TEXT[SIGNUP]}</Link>
+            </NextLink>
+            .
+          </Text>
+        ) : (
+          <Text type="secondary">
+            Already a Complaza user? Please{" "}
+            <NextLink href="/login" passHref>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link>{BUTTON_TEXT[LOGIN]}</Link>
+            </NextLink>
+            .
+          </Text>
+        )}
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          {BUTTON_TEXT[type]}
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+NewSession.propTypes = {
+  type: PropTypes.oneOf([LOGIN, SIGNUP]).isRequired,
+}
+
+export default NewSession
---
+++ pages/signup.js
@@ -0,0 +1,8 @@
+import React from "react"
+import NewSession, { SIGNUP } from "../components/forms/session"
+
+function SignUp(props) {
+  return <NewSession type={SIGNUP} />
+}
+
+export default SignUp
